33a6d8cd8fc334434c2852e343b707ed0ac6e62f
pratibhamaniprajapati <pratibhamani95@gmail.com>
2024-08-13 14:22:57 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginTab = document.getElementById('login-tab');
+    const registerTab = document.getElementById('register-tab');
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const mainImage = document.getElementById('main-image');
+
+    loginTab.addEventListener('click', function() {
+        loginTab.classList.add('active');
+        registerTab.classList.remove('active');
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+        mainImage.src = 'images/image1.png'; // Ensure image1 remains when switching tabs
+    });
+
+    registerTab.addEventListener('click', function() {
+        registerTab.classList.add('active');
+        loginTab.classList.remove('active');
+        registerForm.style.display = 'block';
+        loginForm.style.display = 'none';
+        mainImage.src = 'images/image1.png'; // Ensure image1 remains when switching tabs
+    });
+});
